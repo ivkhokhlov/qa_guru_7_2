f2b21d5abb991f592ce810126af934a5a7c79eb3
Ivan Khokhlov <xxx.mailbox73@gmail.com>
2023-07-27 09:07:40 +0400

    add second test for search

--- test_google_search.py
+++ test_google_search.py
@@ -1,6 +1,12 @@
 from selene import be, have
 
 
+def test_noneempty_search_result(google_page, random_string):
+    browser = google_page
+    browser.element('[type="search"]').should(be.blank).type('github').press_enter()
+    browser.element('[id="res"]').should(have.text("GitHub: Let's build from here · GitHub"))
+
+
 def test_empty_search_result(google_page, random_string):
     browser = google_page
     browser.element('[type="search"]').should(be.blank).type(random_string).press_enter()
